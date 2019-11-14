8024034c69903a80751f5e1670ab3590dd61c0ed
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 05:35:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,9 +37,9 @@ shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest={"gun powder" => 4}
+shipping_manifest = {"gun powder" => 4}
 
 
   # return the shipping_manifest hash below
-shipping_manifest
+return shipping_manifest
 end
