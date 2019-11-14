0c86cc9ca992f24006dbb46147fd558a0c694857
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 05:27:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash={:name =>"Whit", :name => "Kari", :name => "Caitlin"}
   
 end
 
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+the_manifest={}
 
 end
 
